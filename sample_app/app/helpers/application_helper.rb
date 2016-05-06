301c639eab6a5ce072e57f90106172462541be0b
Shotaro Sasaki <fspia.biz@gmail.com>
2016-05-06 18:45:24 +0900

    05/06 continue

--- sample_app/app/helpers/application_helper.rb
+++ sample_app/app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(title = '')
+    base = "Ruby on Rails Tutorial Sample App"
+    if title.empty?
+      base
+    else
+      title + " | " + base
+    end
+  end
+
 end
